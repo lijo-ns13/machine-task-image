5bdb6c22c7d8fbd158e235e7ee61ce7935991e20
lijo ns <lijons13@gmail.com>
2025-07-30 10:29:18 +0530

    repository pattern structure implememted:initial

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+import bcrypt from "bcryptjs";
+
+interface IUser extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: [true, "name  is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is requierd"],
+    unique: [true, "Email already exists"],
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  phoneNumber: {
+    type: Strin,
+  },
+});
