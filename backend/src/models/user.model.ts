4487c075867b7f48ace4d1f0fb54b750ba5f4679
lijo ns <lijons13@gmail.com>
2025-07-31 13:54:33 +0530

    feat(auth) added userSchema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -11,24 +11,49 @@ interface IUser extends Document {
   updatedAt: Date;
 }
 
-const userSchema = new Schema<IUser>({
-  name: {
-    type: String,
-    required: [true, "name  is required"],
-    trim: true,
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, "name  is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is requierd"],
+      unique: [true, "Email already exists"],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    phoneNumber: {
+      type: Number,
+      required: [true, "phone Number is required"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "email is requierd"],
-    unique: [true, "Email already exists"],
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-  },
-  phoneNumber: {
-    type: Strin,
-  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.password) {
+    return next();
+  }
+  if (!user.isModified("password")) {
+    return next();
+  }
+  try {
+    const saltRounds = 10;
+    if (user.password && user.password.startsWith("$2b$")) {
+      return next();
+    }
+    user.password = await bcrypt.hash(user.password, saltRounds);
+  } catch (error) {
+    next(error as Error);
+  }
 });
+export default mongoose.model<IUser>("User", userSchema);
